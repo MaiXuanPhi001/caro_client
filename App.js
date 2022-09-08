5507ea9dbd632cbacc2138d878757ee3e4f9ee26
MaiXuanPhi <101462441+MaiXuanPhi001@users.noreply.github.com>
2022-09-08 13:34:20 +0700

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import CaroTable from './src/components/screens/caros/CaroTable';
+import Hello from './src/components/screens/hellos/Hello';
+import PvsP from './src/components/screens/PvsP/PvsP';
+import Socket from './src/components/screens/Socket/Socket';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <CaroTable />
+    // <Socket />
+    // <PvsP />
+    <Hello />
   );
 }
 
---
+++ src/components/utils/Contants.js
@@ -0,0 +1,3 @@
+export const CONTANT = {
+    hosting: 'http://192.168.1.12:3000'
+}
